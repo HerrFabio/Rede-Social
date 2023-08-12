0b2ec5e61d4cb7794ca86ab8958d9f8b79cd4dc3
Fabio <fabioalbertoaraujo@gmail.com>
2023-08-11 23:56:08 -0300

    Entrega 1 atualizada

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,58 +1,127 @@
-const openPostButtons = document.querySelectorAll('.buttonOpen__post');
+// Dados fornecidos
+export const users = [
+  {
+    id: 1,
+    user: "Samuel Leão",
+    stack: "Front end Engineer",
+    img: "./src/assets/img/user1.svg",
+  },
+  {
+    id: 2,
+    user: "Samuel Persuhn",
+    stack: "Front end Engineer",
+    img: "./src/assets/img/user2.svg",
+  },
+  {
+    id: 3,
+    user: "Carlos Lima",
+    stack: "UX e UI Designer",
+    img: "./src/assets/img/user3.svg",
+  },
+  {
+    id: 4,
+    user: "Carla Maria",
+    stack: "Back end Engineer",
+    img: "./src/assets/img/user4.svg",
+  },
+  {
+    id: 5,
+    user: "Júlia Martins",
+    stack: "Devop's",
+    img: "./src/assets/img/user5.svg",
+  },
+];
 
+export const posts = [
+  {
+    id: 1,
+    title: "Empresa de Tecnologia da Informação abre vagas para programa de estágio",
+    text: "A Framework Digital, empresa mineira especializada em Tecnologia da Informação, irá iniciar o seu sexto programa de estágio, com o prazo de inscrições entre os dias 08 e 28 de agosto. O programa é conhecido como Framework Padawans, com inspiração nos filmes Star Wars. Nas histórias, os iniciantes fazem treinamentos para se tornar cavaleiros Jedi, que compõem o lado bom da força.",
+    user: "Samuel Persuhn",
+    stack: "Front end Engineer",
+    img: "./src/assets/img/user2.svg",
+    likes: 25
+  },
+  {
+    id: 2,
+    title: "Programa de estágio abre vagas em Segurança da Informação com regime remoto",
+    text: "Em Segurança da Informação, as vagas são destinadas às áreas de Security Operations Center (SOC), Administração de Dispositivos de Segurança (ADS), Resposta a Incidentes (RI), Segurança e Privacidade e Consultoria Técnica. O candidato interessado deverá estar matriculado em um curso superior em Ciência da Computação, Segurança da Informação, Tecnologia da Informação e afins, com previsão de conclusão do curso de fevereiro de 2023 a fevereiro de 2025.",
+    user: "Carla Maria",
+    stack: "Back end Engineer",
+    img: "./src/assets/img/user4.svg",
+    likes: 19
+  },
+  {
+    id: 3,
+    title: "O que é programação orientada a objetos e programação funcional",
+    text: "A Programação Funcional é uma orientação focada na programação declarativa. Conhecendo mais a programação funcional a partir de códigos podemos nos deparar de primeira com o conceito mais central da programação funcional, que é o conceito de funções puras, isso significa que o retorno de uma função deve depender apenas dos seus parâmetros de entrada. Com classes podemos editar os valores das propriedades dos objetos criados ou ainda criar métodos para essas propriedades, ainda por cima podemos definir se vão ser públicos (vão para o objeto) ou estáticos (não são instanciados, ou seja, não vão para o objeto), e isso tem seu lado bom e ruim.",
+    user: "Júlia Martins",
+    stack: "Devop's",
+    img: "./src/assets/img/user5.svg",
+    likes: 8
+  },
+];
 
-const modal = document.getElementById('postModal');
-const closeButton = document.querySelector('.modal__closeButton');
+export const suggestUsers = [
+  {
+    id: 1,
+    user: "Carlos Lima",
+    stack: "UX e UI Designer",
+    img: "./src/assets/img/user3.svg",
+  },
+  {
+    id: 2,
+    user: "Júlia Martins",
+    stack: "Devop's",
+    img: "./src/assets/img/user5.svg",
+  },
+  {
+    id: 3,
+    user: "Rafael Bertoldo",
+    stack: "Fullstack Engineer",
+    img: "./src/assets/img/user7.svg",
+  },
+];
 
+import { users, posts, suggestUsers } from './index.js'
 
-function openModal(postData) {
+// Função para abrir o modal
+function openModal(postId) {
+  const postData = posts.find((post) => post.id == postId);
+
+  const modal = document.querySelector("#postModal");
+
+  if (!postData) {
+    console.error(`Post with ID ${postId} not found.`);
+    return;
+  }
+
   const {
-    userName,
-    userImageSrc,
-    userOffice,
-    postTitle,
-    postContent,
+    user: userName,
+    img: userImageSrc,
+    stack: userOffice,
+    title: postTitle,
+    text: postContent,
   } = postData;
 
+  const modalTitle = modal.querySelector(".modal__title");
+  const modalUser = modal.querySelector(".modal__user");
+  const modalImg = modal.querySelector(".modal__img");
+  const modalStack = modal.querySelector(".modal__stack");
+  const modalText = modal.querySelector(".modal__text");
 
-  const modalUserName = modal.querySelector('.card__userName');
-  const modalUserImage = modal.querySelector('.card__image');
-  const modalUserOffice = modal.querySelector('.card__office');
-  const modalPostTitle = modal.querySelector('.modal__postTitle');
-  const modalPostContent = modal.querySelector('.modal__postContent');
-
-  modalUserName.textContent = userName;
-  modalUserImage.src = userImageSrc;
-  modalUserImage.alt = userName;
-  modalUserOffice.textContent = userOffice;
-  modalPostTitle.textContent = postTitle;
-  modalPostContent.textContent = postContent;
-
+  modalTitle.textContent = postTitle;
+  modalUser.textContent = `Author: ${userName}`;
+  modalImg.src = userImageSrc;
+  modalStack.textContent = `Stack: ${userOffice}`;
+  modalText.textContent = postContent;
 
   modal.showModal();
 }
 
 
+// Função para fechar o modal
 function closeModal() {
+  const modal = document.querySelector("#postModal");
   modal.close();
 }
-
-
-openPostButtons.forEach((button) => {
-  button.addEventListener('click', () => {
-   
-    const postData = {
-      userName: 'Nome do Usuário',
-      userImageSrc: './src/assets/img/userX.svg', 
-      userOffice: 'Cargo do Usuário',
-      postTitle: 'Título do Post',
-      postContent: 'Conteúdo do Post...',
-    };
-
-
-    openModal(postData);
-  });
-});
-
-
-closeButton.addEventListener('click', closeModal);
